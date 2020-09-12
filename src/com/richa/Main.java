37eb1ef9801d64809ea218cd66c4496b4e6861d9
Richa Luhtra <richaluhtra.ca@gmail.com>
2020-09-12 04:33:51 -0700

    first commit in pro 1

---
+++ src/com/richa/Main.java
@@ -0,0 +1,23 @@
+package com.richa;
+
+public class Main {
+
+    public static void main(String[] args) {
+	short a=123;
+	short b=232;
+	int c=1234556;
+	int d=1232546;
+	long e=734706639666L;
+	long f=587998320723L;
+	float amount=233.4224F;
+	double balance=234352.4325346452;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(amount);
+        System.out.println(balance);
+    }
+}
